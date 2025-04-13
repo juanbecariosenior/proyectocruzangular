1c34270b936d853ad7c0288d7f174a2fd9a86ecb
juanbecariosenior <juanjimenezbecario@gmail.com>
2025-04-13 16:44:23 -0600

    Update manuales.component.ts
    se edita el archivo ts del componente manuales

--- proyectocruz/src/app/components/manuales/manuales.component.ts
+++ proyectocruz/src/app/components/manuales/manuales.component.ts
@@ -1,11 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-manuales',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './manuales.component.html',
   styleUrl: './manuales.component.scss'
 })
 export class ManualesComponent {
-
+  productos = [
+    { nombre: 'Producto 1', seleccionado: true },
+    { nombre: 'Producto 2', seleccionado: true },
+    { nombre: 'Producto 3', seleccionado: false },
+    { nombre: 'Producto 4', seleccionado: false },
+  ];
+  
+  etiquetas = [
+    { ean: '1111111111111', sku: '1111111111111', tipo: 'J', cantidad: 10000, conPrecio: false },
+    { ean: '1111111111111', sku: '1111111111111', tipo: 'J', cantidad: 10000, conPrecio: false },
+    { ean: '1111111111111', sku: '1111111111111', tipo: 'J', cantidad: 10000, conPrecio: false },
+    { ean: '1111111111111', sku: '1111111111111', tipo: 'J', cantidad: 10000, conPrecio: false },
+  ];
 }
+
