b62f22b5fe5f4e646e8b12f0f0f058b14ba5f129
juanbecariosenior <juanjimenezbecario@gmail.com>
2025-04-13 16:40:42 -0600

    Update automaticas.component.ts
    se modifica el archivo ts de automaticas

--- proyectocruz/src/app/components/automaticas/automaticas.component.ts
+++ proyectocruz/src/app/components/automaticas/automaticas.component.ts
@@ -10,23 +10,16 @@ import { FormsModule } from '@angular/forms';
 })
 export class AutomaticasComponent {
   productos = [
-    { id: 1, nombre: 'Producto 1', seleccionado: true },
-    { id: 2, nombre: 'Producto 2', seleccionado: true },
-    { id: 3, nombre: 'Producto 3', seleccionado: false },
-    { id: 4, nombre: 'Producto 4', seleccionado: false },
+    { nombre: 'Producto 1', seleccionado: true },
+    { nombre: 'Producto 2', seleccionado: true },
+    { nombre: 'Producto 3', seleccionado: false },
+    { nombre: 'Producto 4', seleccionado: false },
   ];
-
-  etiquetas: any[] = [];
-
-  agregarEtiquetas() {
-    this.etiquetas = this.productos
-      .filter((producto) => producto.seleccionado)
-      .map(() => ({
-        ean: '111111111111',
-        sku: '111111111111',
-        tipo: 'J',
-        cantidad: 10000,
-        conPrecio: false,
-      }));
-  }
+  
+  etiquetas = [
+    { ean: '1111111111111', sku: '1111111111111', tipo: 'J', cantidad: 10000, conPrecio: false },
+    { ean: '1111111111111', sku: '1111111111111', tipo: 'J', cantidad: 10000, conPrecio: false },
+    { ean: '1111111111111', sku: '1111111111111', tipo: 'J', cantidad: 10000, conPrecio: false },
+    { ean: '1111111111111', sku: '1111111111111', tipo: 'J', cantidad: 10000, conPrecio: false },
+  ];
 }
